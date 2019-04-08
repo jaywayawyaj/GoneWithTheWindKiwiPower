ccd3007042b5d18a5920e2c45cb2755a70d550e3
Jethro Whaley <jethro.whaley@gmail.com>
2019-04-08 14:52:15 +0100

    refactor: searchTitle updated to cater for multiple query matches

--- src/main/scala/com/company/library/Library.scala
+++ src/main/scala/com/company/library/Library.scala
@@ -2,10 +2,10 @@ package com.company.library
 
 class Library (val books :List[Book] = Books.all) {
 
-  def searchTitle(titleSearch: String): Book = {
-    var result: Book = null
+  def searchTitle(titleSearch: String): Set[Book] = {
+    var result = Set[Book]()
     books.foreach {
-      book => if(book.title.contains(titleSearch)) { result = book }
+      book => if(book.title.contains(titleSearch)) { result += book }
     }
     result
   }
--- src/test/scala/com/company/library/LibrarySpec.scala
+++ src/test/scala/com/company/library/LibrarySpec.scala
@@ -8,8 +8,23 @@ class LibrarySpec extends FunSuite {
   val library = new Library
 
   test("searching part of the name should return the entire book") {
-    library.searchTitle("Azkaban") shouldBe Book(
-      "Harry Potter and the Prisoner of Azkaban", "Rowling, J.K.", "iamvmb"
+    library.searchTitle("Azkaban") shouldBe Set(
+      Book("Harry Potter and the Prisoner of Azkaban", "Rowling, J.K.", "iamvmb"),
     )
   }
+
+  test("searching part of the name should return all books that match query") {
+    println(library.searchTitle("Potter"))
+    library.searchTitle("Potter") shouldEqual Set(
+      Book("Harry Potter and the Deathly Hallows", "Rowling, J.K.", "ipszbehyh"),
+      Book("Harry Potter and the Philosopher's Stone", "Rowling, J.K.", "lfzowqpsj"),
+      Book("Harry Potter and the Order of the Phoenix", "Rowling, J.K.", "ymjklwq"),
+      Book("Harry Potter and the Goblet of Fire", "Rowling, J.K.", "krsmrccb"),
+      Book("Harry Potter and the Chamber of Secrets", "Rowling, J.K.", "dvceqblua"),
+      Book("Harry Potter and the Prisoner of Azkaban", "Rowling, J.K.", "iamvmb"),
+      Book("Harry Potter and the Half-blood Prince:Children's Edition", "Rowling, J.K.", "gdjvia"),
+      Book("Harry Potter and the Half-blood Prince", "Rowling, J.K.", "ajaoshq"),
+    )
+  }
+
 }
