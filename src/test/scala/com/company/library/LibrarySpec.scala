50b7a07446650a1aa3e83382e48cfff47106d2b3
Jethro Whaley <jethro.whaley@gmail.com>
2019-04-08 15:14:02 +0100

    test: test added to check author with multiple titles

--- src/test/scala/com/company/library/LibrarySpec.scala
+++ src/test/scala/com/company/library/LibrarySpec.scala
@@ -32,4 +32,18 @@ class LibrarySpec extends FunSuite {
     )
   }
 
+  test("searching part of the authors name should return all books that match query") {
+    library.searchAuthor("Rowling") shouldEqual Set(
+      Book("Harry Potter and the Deathly Hallows", "Rowling, J.K.", "ipszbehyh"),
+      Book("Harry Potter and the Philosopher's Stone", "Rowling, J.K.", "lfzowqpsj"),
+      Book("Harry Potter and the Order of the Phoenix", "Rowling, J.K.", "ymjklwq"),
+      Book("Harry Potter and the Goblet of Fire", "Rowling, J.K.", "krsmrccb"),
+      Book("Harry Potter and the Chamber of Secrets", "Rowling, J.K.", "dvceqblua"),
+      Book("Harry Potter and the Prisoner of Azkaban", "Rowling, J.K.", "iamvmb"),
+      Book("Harry Potter and the Half-blood Prince:Children's Edition", "Rowling, J.K.", "gdjvia"),
+      Book("Harry Potter and the Half-blood Prince", "Rowling, J.K.", "ajaoshq"),
+      Book("Tales of Beedle the Bard,The", "Rowling, J.K.", "webvafppur")
+    )
+  }
+
 }
