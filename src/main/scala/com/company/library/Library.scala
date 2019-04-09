e8f4464ebfaad2426bb07b3644dc7b2411e768ad
Jethro Whaley <jethro.whaley@gmail.com>
2019-04-09 14:01:12 +0100

    refactor: brought book list back with library class

--- src/main/scala/com/company/library/Library.scala
+++ src/main/scala/com/company/library/Library.scala
@@ -1,12 +1,14 @@
 package com.company.library
 
 class Library (
-                var onLoan :Set[Book] = Set[Book]()
-              ) {
+  val books :List[Book] = Books.all
+  ) {
+
+  var onLoan :Set[Book] = Set[Book]()
 
   def searchTitle(titleSearch: String): Set[Book] = {
     var result = Set[Book]()
-    Books.all.foreach {
+    books.foreach {
       book => if(book.title.contains(titleSearch)) { result += book }
     }
     result
@@ -14,7 +16,7 @@ class Library (
 
   def searchAuthor(authorSearch: String): Set[Book] = {
     var result = Set[Book]()
-    Books.all.foreach {
+    books.foreach {
       book => if(book.author.contains(authorSearch)) { result += book }
     }
     result
@@ -22,14 +24,14 @@ class Library (
 
   def searchISBN(isbnSearch: String): Book = {
     var result :Book = null
-    Books.all.foreach {
-      book => if(book.ISBN == isbnSearch) { result = book}
+    books.foreach {
+      book => if(book.ISBN == isbnSearch) { result = book  }
     }
     result
   }
 
-  def lend(isbn :String) = {
-    Books.all.foreach {
+  def lend(isbn :String) :Unit = {
+    books.foreach {
       book => if(book.ISBN == isbn) { onLoan += book }
     }
   }
