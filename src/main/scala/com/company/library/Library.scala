6cc7254c9042f36bc9607a8d585141b02b45c250
Jethro Whaley <jethro.whaley@gmail.com>
2019-04-10 11:52:45 +0100

    refactor: seperated functions to new file

--- src/main/scala/com/company/library/Library.scala
+++ src/main/scala/com/company/library/Library.scala
@@ -22,21 +22,3 @@ class Library (
   }
 }
 
-object Functions {
-
-  def searchTitle(titleSearch: String) :List[Book] = {
-    val result = Books.all.filter(_.title.contains(titleSearch))
-    result
-  }
-
-  def searchAuthor(authorSearch: String) :List[Book] = {
-    val result = Books.all.filter(_.author.contains(authorSearch))
-    result
-  }
-
-  def searchISBN(isbnSearch: String) :Option[Book] = {
-    val result = Books.all.find(_.ISBN == isbnSearch)
-    result
-  }
-
-}
